ff7e9837b75dfa4c727ac119ddf5f9fdb36dac8b
Shashank Singh <Shashank Singh>
2023-09-16 22:59:27 +0530

    Added the slider input and Change grid color upon hovering functionality

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 "use strict";
 //Get the grid element
 const grid = document.querySelector(".grid");
+const slider = document.getElementById("myRange");
+const range = document.querySelector(".range");
 
+const changeBoxColor = function (e) {
+  this.style.backgroundColor = "black";
+};
+
+const createGrid = function (squares, squareNum, squareSide) {
+  for (let i = 0; i < squareNum; i++) {
+    squares[i] = document.createElement("div");
+    squares[i].classList.add("square");
+    squares[i].setAttribute("id", i);
+    //   squares[i].style.width = "150px";
+    //   squares[i].style.height = "auto";
+    squares[i].style.width = `${squareSide}px`;
+    squares[i].style.height = `${squareSide}px`;
+
+    squares[i].style.border = "1px solid rgb(156, 156, 156)";
+    squares[i].addEventListener("mouseover", changeBoxColor);
+  }
+  for (let j = 0; j < squareNum; j++) {
+    grid.appendChild(squares[j]);
+  }
+};
+
+const updateGrid = function () {
+  grid.innerHTML = "";
+  range.textContent = `${slider.value}✕${slider.value}`;
+  let N = slider.value;
+  let squareNum = N * N;
+  let squareSide = 600 / squareNum ** 0.5;
+  const squares = [];
+  createGrid(squares, squareNum, squareSide);
+};
+
+updateGrid();
+slider.addEventListener("input", updateGrid);
 //create 16 squares
-const N = 20;
-const squareNum = N * N;
-const squareSide = 600 / squareNum ** 0.5;
-
-const squares = [];
-for (let i = 0; i < squareNum; i++) {
-  squares[i] = document.createElement("div");
-  squares[i].classList.add("square");
-  squares[i].setAttribute("id", i);
-  //   squares[i].style.width = "150px";
-  //   squares[i].style.height = "auto";
-  squares[i].style.width = `${squareSide}px`;
-  squares[i].style.height = `${squareSide}px`;
-
-  squares[i].style.border = "1px solid rgb(156, 156, 156)";
-}
-for (let j = 0; j < squareNum; j++) {
-  grid.appendChild(squares[j]);
-}
