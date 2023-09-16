0ca1ca8f6579d584ccb6f2f834639bfe4302f3cb
Shashank Singh <Shashank Singh>
2023-09-16 21:50:51 +0530

    Variable Grid Creation Functionality
    Now We can generate grid by changing the value of N through js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
+"use strict";
+//Get the grid element
+const grid = document.querySelector(".grid");
 
+//create 16 squares
+const N = 20;
+const squareNum = N * N;
+const squareSide = 600 / squareNum ** 0.5;
+
+const squares = [];
+for (let i = 0; i < squareNum; i++) {
+  squares[i] = document.createElement("div");
+  squares[i].classList.add("square");
+  squares[i].setAttribute("id", i);
+  //   squares[i].style.width = "150px";
+  //   squares[i].style.height = "auto";
+  squares[i].style.width = `${squareSide}px`;
+  squares[i].style.height = `${squareSide}px`;
+
+  squares[i].style.border = "1px solid rgb(156, 156, 156)";
+}
+for (let j = 0; j < squareNum; j++) {
+  grid.appendChild(squares[j]);
+}
