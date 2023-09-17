c9a325e7425ad6d14d603c46a89a41cb82d824ff
Shashank Singh <Shashank Singh>
2023-09-17 16:54:07 +0530

    Added the Eraser Logic and Button Highlight logic

--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 "use strict";
-//Get the grid element
 const grid = document.querySelector(".grid");
 const slider = document.getElementById("myRange");
 const range = document.querySelector(".range");
+const blackButton = document.querySelector(".black-button");
+const rainbowButton = document.querySelector(".rainbow-button");
+const eraserButton = document.querySelector(".eraser-button");
+
+let defaultColour = "black";
+
+const rainbowGenerator = function () {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  return `rgb(${red},${green},${blue})`;
+};
+
+blackButton.addEventListener("click", function (e) {
+  defaultColour = "black";
+  this.classList.add("selected");
+  rainbowButton.classList.remove("selected");
+  eraserButton.classList.remove("selected");
+});
+
+rainbowButton.addEventListener("click", function () {
+  defaultColour = "rainbow";
+  this.classList.add("selected");
+  eraserButton.classList.remove("selected");
+  blackButton.classList.remove("selected");
+});
+
+eraserButton.addEventListener("click", function () {
+  defaultColour = "white";
+  this.classList.add("selected");
+  blackButton.classList.remove("selected");
+  rainbowButton.classList.remove("selected");
+});
 
 const changeBoxColor = function (e) {
-  this.style.backgroundColor = "black";
+  if (defaultColour === "rainbow") {
+    this.style.backgroundColor = rainbowGenerator();
+  } else {
+    this.style.backgroundColor = defaultColour;
+  }
 };
 
 const createGrid = function (squares, squareNum, squareSide) {
@@ -13,11 +49,9 @@ const createGrid = function (squares, squareNum, squareSide) {
     squares[i] = document.createElement("div");
     squares[i].classList.add("square");
     squares[i].setAttribute("id", i);
-    //   squares[i].style.width = "150px";
-    //   squares[i].style.height = "auto";
     squares[i].style.width = `${squareSide}px`;
     squares[i].style.height = `${squareSide}px`;
-    squares[i].style.border = "1px solid rgb(156, 156, 156)";
+    // squares[i].style.border = "1px solid rgb(156, 156, 156)";
     squares[i].addEventListener("mouseover", changeBoxColor);
   }
   for (let j = 0; j < squareNum; j++) {
@@ -30,11 +64,10 @@ const updateGrid = function () {
   range.textContent = `${slider.value}✕${slider.value}`;
   let N = slider.value;
   let squareNum = N * N;
-  let squareSide = 600 / squareNum ** 0.5;
+  let squareSide = 500 / squareNum ** 0.5;
   const squares = [];
   createGrid(squares, squareNum, squareSide);
 };
 
 updateGrid();
 slider.addEventListener("input", updateGrid);
-//create 16 squares
